0f5e897197eb69315da7de158f582e729ecf4d8a
sairamsnv <55974736+sairamsnv@users.noreply.github.com>
2021-07-30 15:11:44 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+header=st.beta_container()
+dataset=st.beta_container()
+features=st.beta_container()
+modelTraining=st.beta_container()
+
+
+
+with header:
+    st.title('Welcome To My Fist Data Science Project!!')
+
+
+
+with dataset:
+    st.header('NBA DataSet')
+    nba=pd.read_csv(r'https://raw.githubusercontent.com/sairamsnv/stream/main/myds.csv')
+    data=pd.DataFrame(nba['Team'].value_counts()).head(20)
+    st.bar_chart(data)
+
+
+
